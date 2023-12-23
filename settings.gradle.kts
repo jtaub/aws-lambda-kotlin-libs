111f946e578a84f48d63abace10d152e637b8d92
jtaubner <JakeTaubner@protonmail.com>
2023-12-23 13:02:09 -0500

    gradle updates

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+    id("net.saliman.cobertura") version "4.0.0"
 }
 rootProject.name = "aws-lambda-kotlin-libs"
 
