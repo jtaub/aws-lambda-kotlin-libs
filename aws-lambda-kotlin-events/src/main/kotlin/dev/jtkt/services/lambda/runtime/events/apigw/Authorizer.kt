bc0223210592b239e9f784d2fe99b8cade8f14fd
jtaubner <JakeTaubner@protonmail.com>
2023-12-25 17:25:57 -0500

    iam policy events

--- aws-lambda-kotlin-events/src/main/kotlin/dev/jtkt/services/lambda/runtime/events/apigw/Authorizer.kt
+++ aws-lambda-kotlin-events/src/main/kotlin/dev/jtkt/services/lambda/runtime/events/apigw/Authorizer.kt
@@ -1,44 +1,21 @@
 package dev.jtkt.services.lambda.runtime.events.apigw
 
+import dev.jtkt.services.lambda.runtime.events.apigw.v2.IamPolicyResponse
 import kotlinx.serialization.Serializable
+import com.amazonaws.services.lambda.runtime.CognitoIdentity as ICognitoIdentity
 
 @Serializable
 data class Authorizer(
     val jwt: JWT = JWT(),
-    val lambda: Map<String, LambdaAuthResponse> = emptyMap(),
+    val lambda: Map<String, IamPolicyResponse> = emptyMap(),
     val iam: IAM = IAM(),
-) {
-    @Serializable
-    data class JWT(
-        val claims: Map<String, String> = emptyMap(),
-        val scopes: List<String> = emptyList(),
-    )
-}
-
-@Serializable
-data class LambdaAuthResponse(
-    val principalId: String = "",
-    val policyDocument: PolicyDocument = PolicyDocument(),
-    val context: Map<String, String> = emptyMap(),
 )
 
 @Serializable
-data class PolicyDocument(
-    val version: String = "",
-    val statements: List<PolicyStatement> = emptyList(),
+data class JWT(
+    val claims: Map<String, String> = emptyMap(),
+    val scopes: List<String> = emptyList(),
 )
-
-@Serializable
-data class PolicyStatement(
-    val action: String,
-    val resource: String,
-    val effect: Effect = Effect.ALLOW,
-)
-
-@Serializable
-enum class Effect {
-    ALLOW, DENY,
-}
 
 @Serializable
 data class IAM(
@@ -55,5 +32,8 @@ data class IAM(
         val amr: List<String> = emptyList(),
         val identityId: String = "",
         val identityPoolId: String = "",
-    )
+    ) : ICognitoIdentity {
+        override fun getIdentityId() = identityId
+        override fun getIdentityPoolId() = identityPoolId
+    }
 }
---
+++ aws-lambda-kotlin-events/src/main/kotlin/dev/jtkt/services/lambda/runtime/events/apigw/v2/IamPolicyResponse.kt
@@ -0,0 +1,55 @@
+package dev.jtkt.services.lambda.runtime.events.apigw.v2
+
+import kotlinx.serialization.Serializable
+
+sealed interface IamPolicyResponse {
+
+    val principalId: String
+    val policyDocument: PolicyDocument
+    val context: Map<String, String>
+
+    companion object {
+        const val EXECUTE_API_INVOKE = "execute-api:Invoke"
+        const val VERSION_2012_10_17 = "2012-10-17"
+    }
+}
+
+@Serializable
+data class PolicyDocument(
+    val statement: List<Statement> = emptyList(),
+    val version: String = IamPolicyResponse.VERSION_2012_10_17,
+)
+
+@Serializable
+data class Statement(
+    val action: String = IamPolicyResponse.EXECUTE_API_INVOKE,
+    val condition: Map<String, Map<String, String>> = emptyMap(),
+    val effect: Effect = Effect.ALLOW,
+    val resource: List<String> = emptyList(),
+) {
+    @Serializable
+    enum class Effect(val value: String) {
+        ALLOW("Allow"), DENY("Deny")
+    }
+}
+
+/**
+ * The IAM Policy Response required for API Gateway HTTP APIs
+ */
+@Serializable
+data class HttpApiIamPolicyResponse(
+    override val context: Map<String, String> = emptyMap(),
+    override val policyDocument: PolicyDocument = PolicyDocument(),
+    override val principalId: String = "",
+) : IamPolicyResponse
+
+/**
+ * The IAM Policy Response required for API Gateway REST APIs
+ */
+@Serializable
+data class RestApiIamPolicyResponse(
+    override val context: Map<String, String> = emptyMap(),
+    override val policyDocument: PolicyDocument = PolicyDocument(),
+    override val principalId: String = "",
+    val usageIdentifierKey: String = "",
+) : IamPolicyResponse
