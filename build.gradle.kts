e6aae869637fc000b58b616a9926c19e7b87919f
jtaubner <JakeTaubner@ProtonMail.com>
2024-01-03 00:00:48 -0500

    replace loop with sequence

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,39 +59,28 @@ koverReport {
     }
 }
 
-// IMPORTANT! Must be defined in the plugins block:
-// plugins { id("org.jetbrains.kotlinx.kover") version ... }
-
 tasks.register("printLineCoverage") {
-    group = "verification" // Put into the same group as the `kover` tasks
+    group = "verification"
     dependsOn("koverXmlReport")
     doLast {
         val report = file("build/reports/kover/report.xml")
 
         val doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(report)
         val rootNode = doc.firstChild
-        var childNode = rootNode.firstChild
-
-        var coveragePercent = 0.0
-
-        while (childNode != null) {
-            if (childNode.nodeName == "counter") {
-                val typeAttr = childNode.attributes.getNamedItem("type")
-                if (typeAttr.textContent == "LINE") {
-                    val missedAttr = childNode.attributes.getNamedItem("missed")
-                    val coveredAttr = childNode.attributes.getNamedItem("covered")
-
-                    val missed = missedAttr.textContent.toLong()
-                    val covered = coveredAttr.textContent.toLong()
-
-                    coveragePercent = (covered * 100.0) / (missed + covered)
-
-                    break
-                }
-            }
-            childNode = childNode.nextSibling
+        val nodes = generateSequence(rootNode.firstChild) { it.nextSibling }
+        val coverageNode = nodes.find { node ->
+            node.nodeName == "counter" && node.attributes.getNamedItem("type")?.textContent == "LINE"
         }
 
+        val coveragePercent = coverageNode?.let {
+            val missedAttr = it.attributes.getNamedItem("missed")
+            val coveredAttr = it.attributes.getNamedItem("covered")
+
+            val missed = missedAttr.textContent.toLong()
+            val covered = coveredAttr.textContent.toLong()
+
+            (covered * 100.0) / (missed + covered)
+        } ?: 0.0
         println("%.1f".format(coveragePercent))
     }
 }
